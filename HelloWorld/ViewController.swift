0c3b0ab191696295f773ae7a9fa493d5ff22f72c
AbuYusuf <AbuYusufSW@gmail.com>
2023-03-17 16:39:47 +0300

    Setting parameters view and button

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    
+    @IBOutlet weak var startView: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startView.layer.cornerRadius = 10
+        
+        redView.layer.cornerRadius = 75
+        yellowView.layer.cornerRadius = 75
+        greenView.layer.cornerRadius = 75
+        
+        redView.alpha = 0.5
+        yellowView.alpha = 0.5
+        greenView.alpha = 0.5
     }
 
-
+    @IBAction func startButton(_ sender: Any) {
+        
+    }
+    
 }
 
