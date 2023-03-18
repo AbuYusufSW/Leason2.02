052806a4de38c5cb6c8bb788201f5520504c6b52
AbuYusuf <AbuYusufSW@gmail.com>
2023-03-18 22:06:45 +0300

    Setting of logic

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -19,18 +19,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         startView.layer.cornerRadius = 10
         
-        redView.layer.cornerRadius = 75
-        yellowView.layer.cornerRadius = 75
-        greenView.layer.cornerRadius = 75
+        redView.layer.cornerRadius = 50
+        yellowView.layer.cornerRadius = 50
+        greenView.layer.cornerRadius = 50
         
         redView.alpha = 0.5
         yellowView.alpha = 0.5
         greenView.alpha = 0.5
     }
 
-    @IBAction func startButton(_ sender: Any) {
-        
+    @IBAction func startButton() {
+        startView.setTitle("NEXT", for: .normal)
+        if redView.alpha == 0.5 && yellowView.alpha == 0.5 {
+            greenView.alpha = 0.5
+            yellowView.alpha = 0.5
+            redView.alpha = 1
+        } else if redView.alpha == 1 {
+            yellowView.alpha = 1
+            redView.alpha = 0.5
+        } else if yellowView.alpha == 1 {
+            greenView.alpha = 1
+            yellowView.alpha = 0.5
+        }
     }
-    
+   
 }
 
